69c616e9c3d7fbd44d0bc014685bb0c520c09c96
vips <vipulprajapati2191@gmail.com>
2017-06-11 12:33:32 +0530

    Changed the HTML for the site.

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,loader
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, 'Blog/post_list.html', {})
--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&ay@1sf%evd_j_top!g&+ube61zc!o@+88_dz70w%@l7o$lszv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'vips.pythonanywhere.com']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
